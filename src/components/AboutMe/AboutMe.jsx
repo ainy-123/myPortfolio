5eb9f375464617bebf0beb19a2fbedeb6937c26d
Ainy <kumariainy1308@gmail.com>
2022-10-20 16:11:08 +0530

    Added changes

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,11 +5,10 @@ function AboutMe(){
     return <div id="aboutMe">
         <h1 style={{color:"rgb(8, 8, 32)"}}>About Me</h1>
         <h2 style={{color:"blueviolet"}}>---who i am---</h2>
-        <div id="description"><p>I am Ainy Kumari, hailing from Bihar and currently pursuing my B.Tech in Electronics engineering at IIT BHU. I have been an internee at Qualcomm. I have contributed to various projects under open source Program named GSSoC'21 and was recognised as one of the top performers. I believe, I am an Enthusiastic learner skilled in competitive programming, data structures, algorithms, web development and exploratory data analysis.
-        </p>
+        <div id="description"><p>I am Ainy Kumari, hailing from Bihar and have completed B.Tech in Electronics engineering at IIT BHU. I have been an internee at Qualcomm'21 and received PPO based on the performance and working there as an associate engineer since july 2022. Apart from that, I have also been an internee at TradeX'22, which is online trading start-up and works on the principle of predicting future based on opinion exchange on current world events.
+             </p>
         <br/>
-        <p>
-            I am currently doing an internship in a start-up named TradeX, which is online trading company and works on the principle of predicting future based on opinion exchange on current world events.
+        <p>I have contributed to various projects under open source Program named GSSoC'21 and was recognised as one of the top performers. I believe, I am an Enthusiastic learner skilled in competitive programming, data structures, algorithms, web development and exploratory data analysis.
         </p></div>
     </div>
 }
