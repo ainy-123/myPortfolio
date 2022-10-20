859f84f5047d93ba24074511079b977b25753c12
Ainy <kumariainy1308@gmail.com>
2022-10-20 15:10:03 +0530

    Added changes

--- src/components/Quotes/Quotes.jsx
+++ src/components/Quotes/Quotes.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Quotes.css";
 
-const day= new Date().getDay();
-const quote= day===1?"Be optimistic":day===2?"Know something about Everything and everything about something":day===3?"Be positive":day===4?"Be loyal":day===5?"Be yourself":day===6?"challenge is the key to innovation":"if the product is free,then you are the product";
+// const day= new Date().getDay();
+// const quote= day===1?"Be optimistic":day===2?"Know something about Everything and everything about something":day===3?"Be positive":day===4?"Be loyal":day===5?"Be yourself":day===6?"challenge is the key to innovation":"if the product is free,then you are the product";
 function Quotes(){
     return <div id="quotes">
         {/* <h2>{quote}</h2> */}
